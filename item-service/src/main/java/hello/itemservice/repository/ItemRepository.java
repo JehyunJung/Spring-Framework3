1da18ef83e728be96ebe4feae988df30f6bc6c80
Jehyun Jung <toojey7@gmail.com>
2022-06-29 23:35:23 +0900

    feat: add Item domain, repository

---
+++ item-service/src/main/java/hello/itemservice/repository/ItemRepository.java
@@ -0,0 +1,36 @@
+package hello.itemservice.repository;
+
+import hello.itemservice.domain.Item;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+    private Map<Long, Item> items=new HashMap<>();
+    private static Long sequence=0L;
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        items.put(item.getId(),item);
+        return item;
+    }
+    public Item findById(Long id){
+        return items.get(id);
+    }
+    public List<Item> findAll(){
+        return new ArrayList<>(items.values());
+    }
+    public void updateItem(Long itemId, Item updatedItem){
+        Item item = findById(itemId);
+        item.setItemName(updatedItem.getItemName());
+        item.setPrice(updatedItem.getPrice());
+        item.setQuantity(updatedItem.getQuantity());
+    }
+    public void clearStore(){
+        items.clear();
+    }
+}
---
+++ item-service/src/test/java/hello/itemservice/repository/ItemRepositoryTest.java
@@ -0,0 +1,65 @@
+package hello.itemservice.repository;
+
+import hello.itemservice.domain.Item;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+    ItemRepository itemRepository=new ItemRepository();
+
+    @AfterEach
+    public void afterEach(){
+        itemRepository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        //given
+        Item item=new Item("item1",1000,1);
+
+        //when
+        Item savedItem = itemRepository.save(item);
+        Item findItem = itemRepository.findById(savedItem.getId());
+        //then
+
+        Assertions.assertThat(savedItem).isEqualTo(findItem);
+    }
+
+    @Test
+    public void findAll(){
+        //given
+        Item item=new Item("item1",1000,1);
+        Item item2=new Item("item2",3000,1);
+        //when
+        Item savedItem=itemRepository.save(item);
+        Item savedItem2=itemRepository.save(item2);
+        List<Item> items=  itemRepository.findAll();
+        //then
+
+        Assertions.assertThat(items.size()).isEqualTo(2);
+    }
+    @Test
+    public void updateItem(){
+        //given
+        Item item=new Item("item1",1000,1);
+        Item savedItem=itemRepository.save(item);
+        Long itemId=savedItem.getId();
+
+        //when
+
+        Item updateParam=new Item("item2",2000,2);
+        itemRepository.updateItem(itemId,updateParam);
+
+        Item updatedItem=itemRepository.findById(itemId);
+        //then
+
+        Assertions.assertThat(updateParam.getItemName()).isEqualTo(updatedItem.getItemName());
+        Assertions.assertThat(updateParam.getPrice()).isEqualTo(updatedItem.getPrice());
+        Assertions.assertThat(updateParam.getQuantity()).isEqualTo(updatedItem.getQuantity());
+    }
+}
